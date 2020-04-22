236d3acd98884aade9b7cd8b176bbc401704449a
Eugene Fed <59748404+Eugene-Fed@users.noreply.github.com>
2020-04-22 04:09:41 +0300

    коммит после первой перекомпоновки объектов в сцене для внесения собственных изменений

---
+++ Assets/Scripts/AsteroidScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    public float rotationSpeed;
+    public float minSpeed, maxSpeed;
+    public GameObject asteroidExposion;
+    public GameObject playerExplosion;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody asteroid = GetComponent<Rigidbody>();
+        asteroid.angularVelocity = Random.insideUnitSphere * rotationSpeed;
+
+        float zSpeed = Random.Range(minSpeed, maxSpeed);
+        asteroid.velocity = new Vector3(0, 0, -zSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GameBoundary")
+        {
+            return;
+        }
+        
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, Quaternion.identity);
+        }
+
+        Instantiate(asteroidExposion, transform.position, Quaternion.identity);
+    }
+}
