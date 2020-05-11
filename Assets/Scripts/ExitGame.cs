d99f60cd45b9334ddf27c2fe601290d6acd727c7
Eugene Fed <59748404+Eugene-Fed@users.noreply.github.com>
2020-05-11 21:41:41 +0300

    Добавил кнопку Бургер и кнопку выхода из игры. Добавил скрипт выхода, без настройки логики.

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour
+{
+    //public GameObject btnMnuExit; //delete this variable declatation
+    public GameObject MnuTop;
+
+    void OnMouseDown ()
+    {
+        //btnMnuExit.SetActive (false);
+        Debug.Log("Нажата кнопка Exit");
+        MnuTop.SetActive (false);
+        Application.Quit();
+    }
+    
+}
